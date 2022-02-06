fc058fba2e41e1bb618176b9175a80724542c3de
SatendraPal <palsatyendra9984@gmail.com>
2022-02-06 09:44:56 +0530

    home screen ui changes and gitignore updated

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -4,23 +4,33 @@ import 'package:veridox/Pages/login_page.dart';
 class HomePage extends StatelessWidget {
   static String homePageName = 'homePage';
 
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(9),
-              )
-            ),
-              onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const LogInPage()));
-          }, child: const Text('Log In')),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, LogInPage.logInPageName);
+                },
+                child: const Text('Log In'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
