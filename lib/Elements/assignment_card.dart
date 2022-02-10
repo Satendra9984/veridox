ca2e5fe1bfdb13325d1ff86d53c611509be3a6d7
SatendraPal <palsatyendra9984@gmail.com>
2022-02-10 22:54:58 +0530

    Gesture detection added in assignment

--- lib/Elements/assignment_card.dart
+++ lib/Elements/assignment_card.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:veridox/Pages/assignment_detail_page.dart';
 import 'package:veridox/models/assignment_model.dart';
 
 class AssignmentCard extends StatelessWidget {
@@ -7,18 +9,27 @@ class AssignmentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Consumer<AssignmentModel>(
-          builder: (BuildContext context, assignment, Widget? child) => Column(
-            children: [
-              Text(assignment.caseId),
-              Text(assignment.description),
-              Text(assignment.address),
-              Text(assignment.type),
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          CupertinoPageRoute(builder: (ctx) => AssignmentDetailPage()),
+        );
+      },
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Consumer<AssignmentModel>(
+            builder: (BuildContext context, assignment, Widget? child) =>
+                Column(
+              children: [
+                Text(assignment.caseId),
+                Text(assignment.description),
+                Text(assignment.address),
+                Text(assignment.type),
+              ],
+            ),
           ),
         ),
       ),
