b550783b535a0666757ef13aaa2a11664701b26d
SatendraPal <palsatyendra9984@gmail.com>
2022-02-10 22:55:11 +0530

    Gesture detection added in assignment

---
+++ lib/Pages/assignment_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AssignmentDetailPage extends StatelessWidget {
+  const AssignmentDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Assignemnt details page'),
+      ),
+      body: Container(),
+    );
+  }
+}
