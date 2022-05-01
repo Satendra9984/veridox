a3c9a1523c06a13cfde09915597178c6a45edc12
SatendraPal <palsatyendra9984@gmail.com>
2022-05-01 16:11:04 +0530

    forms 3

--- lib/templates/form.dart
+++ lib/templates/form.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:veridox/templates/form_page.dart';
 
@@ -7,22 +6,25 @@ import '../app_models/saved_assignment_model.dart';
 class Form extends StatefulWidget {
   final SavedAssignment saveAssignment;
 
-  final int pageNo;
-
-  const Form({Key? key, required this.saveAssignment, required this.pageNo})
-      : super(key: key);
+  const Form({Key? key, required this.saveAssignment}) : super(key: key);
 
   @override
   State<Form> createState() => _FormState();
 }
 
 class _FormState extends State<Form> {
+  late final Map<String, dynamic> _formData = widget.saveAssignment.formData;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   List<Widget> _getScreens() {
     List<Widget> screen = [];
     final List<Map<String, dynamic>> jsonPageData =
-        widget.saveAssignment.formData['pages'] as List<Map<String, dynamic>>;
+        _formData['pages'] as List<Map<String, dynamic>>;
 
-    int num = 0;
     for (int i = 0; i < jsonPageData.length; i++) {
       screen.add(
         FormPage(
@@ -43,11 +45,13 @@ class _FormState extends State<Form> {
       ),
       body: PageView(
         controller: _pageController,
-        onPageChanged: (num) {
-          setState(() {
-            // _pageController.jumpToPage(num);
-            currentItemSelected = num;
-          });
+        onPageChanged: (currentScreen) {
+          setState(
+            () {
+              // _pageController.jumpToPage(num);
+              currentItemSelected = currentScreen;
+            },
+          );
         },
         children: _getScreens(),
       ),
